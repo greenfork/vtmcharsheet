a607bceede0d0df7752fab619eb91fa8f37bc5c2
Dmitry Matveyev <dm.matveyev@protonmail.com>
2020-07-18 23:17:01 +0300

    Add carousel

--- src/reag/core.cljs
+++ src/reag/core.cljs
@@ -1,31 +1,55 @@
 (ns reag.core
     (:require
       [reagent.core :as r]
-      [reagent.dom :as d]))
+      [reagent.dom :as d]
+      [clojure.string :as str]))
 
 ;; -------------------------
 ;; State
 
 (defonce charsheet (r/atom {}))
+(defonce page (r/atom 0))
 
 ;; -------------------------
 ;; Views
 
 (defn aligned-text-input [label]
    [:div.pure-control-group
-    [:label {:for label} (clojure.string/capitalize label)]
-    [:input {:name label :id label
-             :on-blur
+    [:label {:for label} (str/capitalize label)]
+    [:input {:name label :id label :value ((keyword label) @charsheet)
+             :on-change
              #(swap! charsheet assoc (keyword label) (.. % -target -value))}]])
 
-(defn home-page []
-  [:div.pure-form.pure-form-aligned
-   [:fieldset
+(defn page-id [id] (str "page" id))
+
+(def pages
+  [
+   ;; First page
+   [:div.pure-form.pure-form-aligned [:fieldset]
     [aligned-text-input "name"]
     [aligned-text-input "player"]
     [aligned-text-input "chronicle"]
     [aligned-text-input "concept"]
-    [aligned-text-input "ambition"]]])
+    [aligned-text-input "ambition"]]
+   ;; Second page
+   [:div "second page"]
+   ;; Third page
+   [:div "third page"]])
+
+(defn has-next-page? [] (< @page (dec (count pages))))
+(defn has-prev-page? [] (> @page 0))
+(defn next-page [] (when (has-next-page?) (swap! page inc)))
+(defn prev-page [] (when (has-prev-page?) (swap! page dec)))
+
+(defn home-page []
+  [:div
+   (nth pages @page [:div "Page not found"])
+   [:button.pure-button
+    {:on-click prev-page :disabled (not (has-prev-page?))}
+    "Previous"]
+   [:button.pure-button
+    {:on-click next-page :disabled (not (has-next-page?))}
+    "Next"]])
 
 ;; -------------------------
 ;; Initialize app
